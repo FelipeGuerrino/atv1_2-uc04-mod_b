24a8c226f016a6f2f92009ff707d66f2a80c440e
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-02-22 21:14:08 -0300

    partial view modal

--- projetohotsite/wwwroot/js/site.js
+++ projetohotsite/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$("#agenda").on("click", (e) => {
+    $("#modalAgenda").modal("show");
+    e.preventDefault();
+});
+$("#dica").on("click", (e) => {
+    $("#modalDica").modal("show");
+    e.preventDefault();
+});
+$("#apoiadores").on("click", (e) => {
+    $("#modalApoiadores").modal("show");
+    e.preventDefault();
+});
